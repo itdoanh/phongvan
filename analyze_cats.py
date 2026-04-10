0050564befed3bf91927ff7e4b7cd369bc077984
Đoàn Văn Doanh <157219955+itdoanh@users.noreply.github.com>
2026-04-10 04:09:44 +0000

    Refine adaptive interview layout and remove temp scripts

--- analyze_cats.py
+++
@@ -1,7 +0,0 @@
-import json
-
-with open('questions.json', 'r', encoding='utf-8') as f:
-    questions = json.load(f)
-
-for q in questions[:100:10]: # Print every 10th question to get a sense of contents
-    print(f"{q['id']} - {q['text'][:50]}...")
